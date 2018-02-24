2e88adf6bc97503c5d8214c98ed7b55ce519069c
Matteo Formentin <matteo.formentin@icloud.com>
2018-02-24 18:02:24 +0100

    Seconda Versione codice

--- project_reti_logiche/project_reti_logiche.sim/sim_1/behav/xsim/xsim.dir/project_tb_behav/webtalk/xsim_webtalk.tcl
+++ project_reti_logiche/project_reti_logiche.sim/sim_1/behav/xsim/xsim.dir/project_tb_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
-webtalk_init -webtalk_dir C:/Progetto reti logiche/project_reti_logiche/project_reti_logiche.sim/sim_1/behav/xsim/xsim.dir/project_tb_behav/webtalk/
+webtalk_init -webtalk_dir C:/Progetto_reti_logiche/project_reti_logiche/project_reti_logiche.sim/sim_1/behav/xsim/xsim.dir/project_tb_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Wed Feb 21 18:26:33 2018" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Sat Feb 24 17:45:13 2018" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2017.4 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "2086221" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -14,11 +14,11 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "879f2161-aa36-4b52-9b4f-64aa7a16f5f3" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "0768c9d3d970421fad8df8aef60be9ad" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "24" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "5" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Microsoft Windows 8 or later , 64-bit" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "Intel(R) Core(TM) i7-7567U CPU @ 3.50GHz" -context "user_environment"
-webtalk_add_data -client project -key cpu_speed -value "3504 MHz" -context "user_environment"
+webtalk_add_data -client project -key cpu_speed -value "3503 MHz" -context "user_environment"
 webtalk_add_data -client project -key total_processors -value "1" -context "user_environment"
 webtalk_add_data -client project -key system_ram -value "4.000 GB" -context "user_environment"
 webtalk_register_client -client xsim
@@ -30,13 +30,13 @@ webtalk_add_data -client xsim -key SDFModeling -value "false" -context "xsim\\co
 webtalk_add_data -client xsim -key HWCosim -value "false" -context "xsim\\command_line_options"
 webtalk_add_data -client xsim -key DPI_Used -value "false" -context "xsim\\command_line_options"
 webtalk_add_data -client xsim -key Debug -value "typical" -context "xsim\\command_line_options"
-webtalk_add_data -client xsim -key Simulation_Image_Code -value "77 KB" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Simulation_Image_Code -value "79 KB" -context "xsim\\usage"
 webtalk_add_data -client xsim -key Simulation_Image_Data -value "10 KB" -context "xsim\\usage"
 webtalk_add_data -client xsim -key Total_Nets -value "0" -context "xsim\\usage"
 webtalk_add_data -client xsim -key Total_Processes -value "4" -context "xsim\\usage"
 webtalk_add_data -client xsim -key Total_Instances -value "8" -context "xsim\\usage"
 webtalk_add_data -client xsim -key Xilinx_HDL_Libraries_Used -value "secureip " -context "xsim\\usage"
-webtalk_add_data -client xsim -key Compiler_Time -value "0.62_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Compiler_Memory -value "47092_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 903426896 -regid "" -xml C:/Progetto reti logiche/project_reti_logiche/project_reti_logiche.sim/sim_1/behav/xsim/xsim.dir/project_tb_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Progetto reti logiche/project_reti_logiche/project_reti_logiche.sim/sim_1/behav/xsim/xsim.dir/project_tb_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Progetto reti logiche/project_reti_logiche/project_reti_logiche.sim/sim_1/behav/xsim/xsim.dir/project_tb_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key Compiler_Time -value "0.64_sec" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Compiler_Memory -value "47084_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 216940807 -regid "" -xml C:/Progetto_reti_logiche/project_reti_logiche/project_reti_logiche.sim/sim_1/behav/xsim/xsim.dir/project_tb_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Progetto_reti_logiche/project_reti_logiche/project_reti_logiche.sim/sim_1/behav/xsim/xsim.dir/project_tb_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Progetto_reti_logiche/project_reti_logiche/project_reti_logiche.sim/sim_1/behav/xsim/xsim.dir/project_tb_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
--- project_reti_logiche/project_reti_logiche.srcs/sources_1/new/project_reti_logiche.vhd
+++ project_reti_logiche/project_reti_logiche.srcs/sources_1/new/project_reti_logiche.vhd
@@ -48,15 +48,15 @@ entity project_reti_logiche is
     );
 end project_reti_logiche;
 
-
 architecture Behavioral of project_reti_logiche is
-    type state_type is (START_WAIT, LETTURA_HEADER, LETTURA_BYTE, CHECK_PIXEL, CALC_AREA, DONE);
+    type state_type is (START_WAIT, AUMENTA_INDIRIZZO, WAIT_CLOCK_CICLE, LETTURA_BYTE,
+      CHECK_PIXEL, CALC_AREA, DONE);
     signal state : state_type;
 begin
-    process(i_clk, i_start, i_rst)
+    process(i_clk, i_rst)
         variable N_COLONNE, N_RIGHE, curr_riga, curr_colonna, SOGLIA : integer;        
         variable current_address : std_logic_vector(15 downto 0);
-        variable letto : integer;
+        variable pixel_corrente : integer;
         variable x_min, y_min :integer :=99999;
         variable x_max, y_max :integer :=0;
         variable area: std_logic_vector(15 downto 0);
@@ -64,80 +64,129 @@ begin
     begin
         if (i_rst = '1') then
             state <= START_WAIT;
-        --elsif (rising_edge(i_clk)) then
-        elsif (i_clk='1' and i_clk'event) then
+        end if;
+        if (rising_edge(i_clk)) then
+       -- if (i_clk='1' and i_clk'event) then
             case state is
                 when START_WAIT =>
-                    if (i_start = '1') then
-                        state <= LETTURA_HEADER;
-                    end if;
-                    
-                when LETTURA_HEADER =>
-                    current_address := "0000000000000000" + "0000000000000010"; --Byte 2
-                    o_address <= current_address;
-                    o_en <= '1';
-                    o_we <= '0';
-                    N_COLONNE := conv_integer(i_data);
-                    
+                    if (i_start = '1') then --INIZIALIZZAZIONE 
+                        current_address := "0000000000000000";
+                        state <= AUMENTA_INDIRIZZO;
+                    end if; 
                    
-                    
-                    current_address := current_address  + "0000000000000001";
-                    o_address <= current_address;
-                    N_RIGHE := conv_integer(i_data);
-                    
-                    current_address := current_address  + "0000000000000001";
-                    o_address <= current_address;
-                    SOGLIA := conv_integer(i_data);
-                    
-                    curr_riga:=0;
-                    curr_colonna:=0;
-                    
-                    o_en <= '0';   
-                    state <= LETTURA_BYTE;
-                    
-                    report "numero colonne: ";
-                    report integer'image(N_COLONNE);
-                    
-                    report "numero righe: ";
-                    report integer'image(N_RIGHE);
-                    
-                    report "numero colonne: ";
-                    report integer'image(SOGLIA);
-                    
-                when LETTURA_BYTE =>
-                    current_address := current_address  + "0000000000000001";
-                    if (conv_integer(current_address) = N_RIGHE + N_COLONNE + 5) then
-                        state <= CALC_AREA;
-                    else
-                        o_address <= current_address;
+                when AUMENTA_INDIRIZZO =>
+                    report "stato: AUMENTA_INDIRIZZO";
+                    --LETTURA N_COLONNE
+                    if( conv_integer(current_address)= 0) then 
                         o_en <= '1';
                         o_we <= '0';
-                        letto := conv_integer(i_data);
-                        curr_colonna:=curr_colonna+1;
-                        if (curr_colonna > N_COLONNE) then
-                            curr_riga:=curr_riga+1;
-                        end if;
-                        state <= CHECK_PIXEL;
+                        current_address := current_address + "0000000000000010";
+                        o_address <= current_address;
+                        state <= WAIT_CLOCK_CICLE;
+                    
+
+                    --LETTURA N_RIGHE
+                    elsif( conv_integer(current_address)= 2) then 
+                        o_en <= '1';
+                        o_we <= '0';
+                        current_address := current_address + "0000000000000001";
+                        o_address <= current_address;
+                        state <= WAIT_CLOCK_CICLE;
+                    
+                    
+                     --LETTURA SOGLIA
+                   else if( conv_integer(current_address)= 3) then 
+                       o_en <= '1';
+                       o_we <= '0';
+                       current_address := current_address + "0000000000000001";
+                        o_address <= current_address;
+                        state <= WAIT_CLOCK_CICLE;
+                   
+                   else if( conv_integer(current_address) > 3 and conv_integer(current_address)< N_RIGHE * N_COLONNE +5) then 
+                        o_en <= '1';
+                        o_we <= '0';
+                        current_address := current_address + "0000000000000001";
+                        o_address <= current_address;
+                        state <= WAIT_CLOCK_CICLE;
+                   
+                    else if(conv_integer(current_address)>= N_RIGHE * N_COLONNE +5 ) then 
+                        state <= CALC_AREA;
+                    end if;
+                    end if;
+                    end if;
                     end if;
                     
+                when LETTURA_BYTE =>
+                     report "stato: WAIT_CLOCK_CICLE";
+                     state <= LETTURA_BYTE;
+             
+                when WAIT_CLOCK_CICLE =>
+                    report "stato: LETTURA_BYTE indirizzo:";
+                    report integer'image(conv_integer(current_address));
+                    
+                    --LETTURA N_COLONNE
+                     if( conv_integer(current_address)= 2) then      
+                        N_COLONNE := conv_integer(i_data);
+                        report "numero colonne: ";
+                        report integer'image(N_COLONNE);
+                        o_en <= '0';
+                        state <= AUMENTA_INDIRIZZO;
+                     end if;
+                     
+                     --LETTURA N_RIGHE
+                     if( conv_integer(current_address) = 3) then                     
+                        N_RIGHE := conv_integer(i_data);
+                        report "numero righe: ";
+                        report integer'image(N_RIGHE);
+                        o_en <= '0';
+                        state <= AUMENTA_INDIRIZZO;
+                      end if;
+                      
+                     --LETTURA SOGLIA
+                     if( conv_integer(current_address) = 4) then                     
+                        SOGLIA := conv_integer(i_data);
+                        report "numero soglia: ";
+                        report integer'image(SOGLIA);
+                        o_en <= '0';
+--                        if(SOGLIA = 0) then
+--                            state <= CALC_AREA;
+--                        else 
+--                            state <= AUMENTA_INDIRIZZO;
+--                        end if; 
+                        state <= AUMENTA_INDIRIZZO;
+                     end if;
+                    
+                    --LETTURA PIXEL
+                     if( conv_integer(current_address)> 4) then                     
+                        pixel_corrente := conv_integer(i_data);
+                        report "valore pixel: ";
+                        report integer'image(pixel_corrente);
+                        o_en <= '0';
+                        state <= CHECK_PIXEL;
+                     end if;
+                                                    
                 when CHECK_PIXEL =>
-                    if (letto >= SOGLIA) then
-                        if (curr_riga < x_min) then
-                            x_min:=curr_riga;
-                        end if;
-                        if (curr_colonna < y_min) then
-                            y_min:=curr_colonna;
-                        end if;
-                        if (curr_riga > x_max) then
-                            x_max:=curr_riga;
-                        end if;
-                        if (curr_colonna > y_max) then
-                            y_max:=curr_colonna;
-                        end if;
-                    end if;
-                    state <= LETTURA_BYTE;
+                    report "stato: CHECK_PIXEL ";
+--                    if (pixel_corrente >= SOGLIA) then
+--                        curr_colonna:=conv_integer(current_address) mod N_COLONNE;
+--                        curr_riga:=(conv_integer(current_address)-5-curr_colonna)/N_COLONNE;
+--                        if (curr_riga < x_min) then
+--                            x_min:=curr_riga;
+--                        end if;
+--                        if (curr_colonna < y_min) then
+--                            y_min:=curr_colonna;
+--                        end if;
+--                        if (curr_riga > x_max) then
+--                            x_max:=curr_riga;
+--                        end if;
+--                        if (curr_colonna > y_max) then
+--                            y_max:=curr_colonna;
+--                        end if;
+--                    end if;
+                    state <= AUMENTA_INDIRIZZO;
                     
                 when CALC_AREA =>
+                     report "stato: CALC_AREA ";
                     area:= std_logic_vector(to_unsigned((x_max-x_min)*(y_max-y_min),16));
                     
                     --Scrittura in memoria area
@@ -151,15 +200,16 @@ begin
                     --Scrittura Byte meno significativo
                     o_address <= "0000000000000000"; 
                     o_data <= area (7 downto 0);
-                    o_en <= '0';   
+                    o_en <= '0';
                     
                     o_done <= '1';
                     state <= DONE;
                     
                 when DONE =>
+                    report "stato: DONE ";
                     o_done <= '0';
                     
-           end case;  
-        end if;
+           end case;
+        end if;  
     end process;  
 end Behavioral;
