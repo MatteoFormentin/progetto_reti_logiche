97e132fad1e1eaf0e018b3e63ea37c258e0e29a1
Matteo Formentin <matteo.formentin@icloud.com>
2018-03-02 00:09:09 +0100

    Aggiunta scrittura risultato in memoria

--- project_reti_logiche/project_reti_logiche.srcs/sources_1/new/project_reti_logiche.vhd
+++ project_reti_logiche/project_reti_logiche.srcs/sources_1/new/project_reti_logiche.vhd
@@ -24,16 +24,6 @@ USE ieee.std_logic_1164.ALL;
 USE ieee.std_logic_unsigned.all;
 USE ieee.numeric_std.ALL;
 
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity project_reti_logiche is
     Port (
            i_clk : in std_logic;
@@ -50,12 +40,13 @@ end project_reti_logiche;
 
 architecture Behavioral of project_reti_logiche is
     type state_type is (START_WAIT, AUMENTA_INDIRIZZO, WAIT_CLOCK_CICLE, LETTURA_BYTE,
-      CHECK_PIXEL, CALC_AREA, DONE);
+      CHECK_PIXEL, CALC_AREA, MSB_WRITE, LSB_WRITE, DONE_HIGH, DONE_LOW);
     signal state : state_type;
 begin
     process(i_clk, i_rst)
         variable N_COLONNE, N_RIGHE, curr_riga, curr_colonna, SOGLIA : integer;        
         variable current_address : std_logic_vector(15 downto 0);
+        variable wait_check : integer :=0;
         variable pixel_corrente : integer;
         variable x_min, y_min :integer :=99999;
         variable x_max, y_max :integer :=0;
@@ -83,7 +74,6 @@ begin
                         current_address := current_address + "0000000000000010";
                         o_address <= current_address;
                         state <= WAIT_CLOCK_CICLE;
-                    
 
                     --LETTURA N_RIGHE
                     elsif( conv_integer(current_address)= 2) then 
@@ -93,34 +83,37 @@ begin
                         o_address <= current_address;
                         state <= WAIT_CLOCK_CICLE;
                     
-                    
                      --LETTURA SOGLIA
-                   else if( conv_integer(current_address)= 3) then 
+                     elsif( conv_integer(current_address)= 3) then 
                        o_en <= '1';
                        o_we <= '0';
                        current_address := current_address + "0000000000000001";
                         o_address <= current_address;
                         state <= WAIT_CLOCK_CICLE;
                    
-                   else if( conv_integer(current_address) > 3 and conv_integer(current_address)< N_RIGHE * N_COLONNE +5) then 
+                      elsif( conv_integer(current_address) > 3 and conv_integer(current_address)< N_RIGHE * N_COLONNE +5) then 
                         o_en <= '1';
                         o_we <= '0';
                         current_address := current_address + "0000000000000001";
                         o_address <= current_address;
                         state <= WAIT_CLOCK_CICLE;
                    
-                    else if(conv_integer(current_address)>= N_RIGHE * N_COLONNE +5 ) then 
+                      elsif(conv_integer(current_address)>= N_RIGHE * N_COLONNE +5 ) then 
                         state <= CALC_AREA;
-                    end if;
-                    end if;
-                    end if;
-                    end if;
-                    
-                when LETTURA_BYTE =>
+                      end if;
+                      
+                when WAIT_CLOCK_CICLE=>
                      report "stato: WAIT_CLOCK_CICLE";
+--                     wait_check:=wait_check+1;
+--                     if (wait_check >1) then 
+--                        wait_check := 0;
+--                        state <= LETTURA_BYTE;
+--                     else 
+--                        state <= WAIT_CLOCK_CICLE;
+--                     end if;
                      state <= LETTURA_BYTE;
              
-                when WAIT_CLOCK_CICLE =>
+                when LETTURA_BYTE =>
                     report "stato: LETTURA_BYTE indirizzo:";
                     report integer'image(conv_integer(current_address));
                     
@@ -130,16 +123,24 @@ begin
                         report "numero colonne: ";
                         report integer'image(N_COLONNE);
                         o_en <= '0';
-                        state <= AUMENTA_INDIRIZZO;
+                        if(N_COLONNE = 0) then
+                            state <= CALC_AREA;
+                        else 
+                            state <= AUMENTA_INDIRIZZO;
+                        end if;
                      end if;
                      
                      --LETTURA N_RIGHE
-                     if( conv_integer(current_address) = 3) then                     
+                     if( conv_integer(current_address) = 3) then
                         N_RIGHE := conv_integer(i_data);
                         report "numero righe: ";
                         report integer'image(N_RIGHE);
                         o_en <= '0';
-                        state <= AUMENTA_INDIRIZZO;
+                        if(N_RIGHE = 0) then
+                            state <= CALC_AREA;
+                        else 
+                            state <= AUMENTA_INDIRIZZO;
+                        end if;
                       end if;
                       
                      --LETTURA SOGLIA
@@ -148,15 +149,14 @@ begin
                         report "numero soglia: ";
                         report integer'image(SOGLIA);
                         o_en <= '0';
---                        if(SOGLIA = 0) then
---                            state <= CALC_AREA;
---                        else 
---                            state <= AUMENTA_INDIRIZZO;
---                        end if; 
-                        state <= AUMENTA_INDIRIZZO;
+                        if(SOGLIA = 0 or SOGLIA = 255) then
+                            state <= CALC_AREA;
+                        else 
+                            state <= AUMENTA_INDIRIZZO;
+                        end if; 
                      end if;
                     
-                    --LETTURA PIXEL
+                     --LETTURA PIXEL
                      if( conv_integer(current_address)> 4) then                     
                         pixel_corrente := conv_integer(i_data);
                         report "valore pixel: ";
@@ -167,45 +167,61 @@ begin
                                                     
                 when CHECK_PIXEL =>
                     report "stato: CHECK_PIXEL ";
---                    if (pixel_corrente >= SOGLIA) then
---                        curr_colonna:=conv_integer(current_address) mod N_COLONNE;
---                        curr_riga:=(conv_integer(current_address)-5-curr_colonna)/N_COLONNE;
---                        if (curr_riga < x_min) then
---                            x_min:=curr_riga;
---                        end if;
---                        if (curr_colonna < y_min) then
---                            y_min:=curr_colonna;
---                        end if;
---                        if (curr_riga > x_max) then
---                            x_max:=curr_riga;
---                        end if;
---                        if (curr_colonna > y_max) then
---                            y_max:=curr_colonna;
---                        end if;
---                    end if;
+                    if (pixel_corrente >= SOGLIA) then
+                        curr_colonna:=conv_integer(current_address) mod N_COLONNE;
+                        curr_riga:=(conv_integer(current_address)- 5 -curr_colonna)/N_COLONNE;
+                        if (curr_riga < x_min) then
+                            x_min:=curr_riga;
+                        end if;
+                        if (curr_colonna < y_min) then
+                            y_min:=curr_colonna;
+                        end if;
+                        if (curr_riga > x_max) then
+                            x_max:=curr_riga;
+                        end if;
+                        if (curr_colonna > y_max) then
+                            y_max:=curr_colonna;
+                        end if;
+                    end if;
                     state <= AUMENTA_INDIRIZZO;
                     
                 when CALC_AREA =>
-                     report "stato: CALC_AREA ";
-                    area:= std_logic_vector(to_unsigned((x_max-x_min)*(y_max-y_min),16));
-                    
-                    --Scrittura in memoria area
+                    report "stato: CALC_AREA ";
+                    --Calcolo Area
+                    if(SOGLIA = 0) then
+                        area:=std_logic_vector(to_unsigned(N_COLONNE*N_RIGHE,16));
+                    elsif(SOGLIA = 255 or N_COLONNE = 0 or N_RIGHE = 0) then
+                        area:="0000000000000000";
+                    else
+                        area:= std_logic_vector(to_unsigned((x_max-x_min)*(y_max-y_min),16));
+                    end if;
+                    report "valore area: ";
+                    report integer'image(conv_integer(area));
+                    state <= MSB_WRITE;
+                
+                when MSB_WRITE =>
                     o_en <= '1';
                     o_we <= '1';
                     
                     --Scrittura Byte più significativo
                     o_address<="0000000000000001"; 
+                    --o_data <= "00000010";
                     o_data <= area (15 downto 8);
+                    state <= LSB_WRITE;
                     
+               when LSB_WRITE =>
                     --Scrittura Byte meno significativo
                     o_address <= "0000000000000000"; 
+                    --o_data<="00000001";
                     o_data <= area (7 downto 0);
-                    o_en <= '0';
+                    state <= DONE_HIGH;
                     
-                    o_done <= '1';
-                    state <= DONE;
+                when DONE_HIGH =>
+                     o_en <= '0';
+                     o_done <= '1';
+                     state <= DONE_LOW;
                     
-                when DONE =>
+                when DONE_LOW =>
                     report "stato: DONE ";
                     o_done <= '0';
                     
