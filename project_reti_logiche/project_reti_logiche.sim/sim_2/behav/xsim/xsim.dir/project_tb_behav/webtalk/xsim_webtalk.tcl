e257a67e41cba16cd4731234a11910e07c8cf3ca
Matteo Formentin <matteo.formentin@icloud.com>
2018-03-03 01:09:27 +0100

    Ora dopo aver alzato done il componente rtorna ad aspettare lo start. Commentato il codice

--- project_reti_logiche/project_reti_logiche.sim/sim_2/behav/xsim/xsim.dir/project_tb_behav/webtalk/xsim_webtalk.tcl
+++ project_reti_logiche/project_reti_logiche.sim/sim_2/behav/xsim/xsim.dir/project_tb_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir C:/Progetto_Reti_Logiche/project_reti_logiche/project_reti_logiche.sim/sim_2/behav/xsim/xsim.dir/project_tb_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Fri Mar  2 17:40:44 2018" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Sat Mar  3 01:07:00 2018" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2017.4 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "2086221" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -18,7 +18,7 @@ webtalk_add_data -client project -key project_iteration -value "11" -context "so
 webtalk_add_data -client project -key os_name -value "Microsoft Windows 8 or later , 64-bit" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "Intel(R) Core(TM) i7-7567U CPU @ 3.50GHz" -context "user_environment"
-webtalk_add_data -client project -key cpu_speed -value "3505 MHz" -context "user_environment"
+webtalk_add_data -client project -key cpu_speed -value "3503 MHz" -context "user_environment"
 webtalk_add_data -client project -key total_processors -value "1" -context "user_environment"
 webtalk_add_data -client project -key system_ram -value "4.000 GB" -context "user_environment"
 webtalk_register_client -client xsim
@@ -26,7 +26,7 @@ webtalk_add_data -client xsim -key Command -value "xsim" -context "xsim\\command
 webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\usage"
 webtalk_add_data -client xsim -key runtime -value "412500 ps" -context "xsim\\usage"
 webtalk_add_data -client xsim -key iteration -value "1" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Time -value "0.05_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Memory -value "10848_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 1364745717 -regid "" -xml C:/Progetto_Reti_Logiche/project_reti_logiche/project_reti_logiche.sim/sim_2/behav/xsim/xsim.dir/project_tb_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Progetto_Reti_Logiche/project_reti_logiche/project_reti_logiche.sim/sim_2/behav/xsim/xsim.dir/project_tb_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Progetto_Reti_Logiche/project_reti_logiche/project_reti_logiche.sim/sim_2/behav/xsim/xsim.dir/project_tb_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key Simulation_Time -value "0.08_sec" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Simulation_Memory -value "10832_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 3058607682 -regid "" -xml C:/Progetto_Reti_Logiche/project_reti_logiche/project_reti_logiche.sim/sim_2/behav/xsim/xsim.dir/project_tb_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Progetto_Reti_Logiche/project_reti_logiche/project_reti_logiche.sim/sim_2/behav/xsim/xsim.dir/project_tb_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Progetto_Reti_Logiche/project_reti_logiche/project_reti_logiche.sim/sim_2/behav/xsim/xsim.dir/project_tb_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
--- project_reti_logiche/project_reti_logiche.srcs/sources_1/new/project_reti_logiche.vhd
+++ project_reti_logiche/project_reti_logiche.srcs/sources_1/new/project_reti_logiche.vhd
@@ -45,38 +45,33 @@ architecture Behavioral of project_reti_logiche is
     signal state : state_type;
 begin
     process(i_clk, i_rst)
-        variable N_COLONNE, N_RIGHE, SOGLIA : integer;        
-        variable curr_riga, curr_colonna: integer :=-1;       
-        variable current_address : std_logic_vector(15 downto 0);
-        variable wait_check : integer :=0;
-        variable pixel_corrente : integer;
-        variable x_min, y_min :integer :=99999;
-        variable x_max, y_max :integer :=0;
-        variable altezza, lunghezza, area_int :integer;
-        variable area: std_logic_vector(15 downto 0);
+        variable N_COLONNE, N_RIGHE, SOGLIA : integer;              --Variabili che contengono i valori dell'header     
+        variable curr_riga, curr_colonna: integer :=-1;             --Variabili che tengono traccia della riga e colonna corrente
+        variable current_address : std_logic_vector(15 downto 0);   --Variabili che contengono l'indirizzo di lettura attuale della RAM
+        variable pixel_corrente : integer;                          --Variabili che contiene il valore dell'ultimo pixel letto
+        variable x_min, y_min :integer :=70000;                     --Variabili che contengono gli indirizzi di colonna più esterni dell'immagine
+        variable x_max, y_max :integer :=0;                         --Variabili che contengono gli indirizzi di riga più esterni dell'immagine
+        variable altezza, lunghezza, area_int :integer;             --Variabili usate per il calcolo finale dell'immagine
+        variable area: std_logic_vector(15 downto 0);               --Variabili che contiene il valore finale binario dell'area da scrivere
         
     begin
-        if (i_rst = '1') then
-            --report "RESET RICEVUTO";
+        if (i_rst = '1') then --Controllo segnale di reset -Asincrono-
             state <= START_WAIT;
         end if;
-        if (rising_edge(i_clk)) then
-       -- if (i_clk='1' and i_clk'event) then
+        if (rising_edge(i_clk)) then --Sincronizzazione sul fronte di salita del clock
             case state is
-                when START_WAIT =>
-                    if (i_start = '1') then --INIZIALIZZAZIONE 
-                            --report "stato START_WAIT";
+                when START_WAIT => 
+                    if (i_start = '1') then --Attendi il segnsle di start
                         current_address := "0000000000000000";
                         state <= AUMENTA_INDIRIZZO;
                     end if; 
                    
                 when AUMENTA_INDIRIZZO =>
-                   -- report "stato: AUMENTA_INDIRIZZO";
                     --LETTURA N_COLONNE
                     if( conv_integer(current_address)= 0) then 
                         o_en <= '1';
                         o_we <= '0';
-                        current_address := current_address + "0000000000000010";
+                        current_address := current_address + "0000000000000010"; --2
                         o_address <= current_address;
                         state <= WAIT_CLOCK_CICLE;
 
@@ -84,7 +79,7 @@ begin
                     elsif( conv_integer(current_address)= 2) then 
                         o_en <= '1';
                         o_we <= '0';
-                        current_address := current_address + "0000000000000001";
+                        current_address := current_address + "0000000000000001"; --3
                         o_address <= current_address;
                         state <= WAIT_CLOCK_CICLE;
                     
@@ -92,7 +87,7 @@ begin
                      elsif( conv_integer(current_address)= 3) then 
                        o_en <= '1';
                        o_we <= '0';
-                       current_address := current_address + "0000000000000001";
+                       current_address := current_address + "0000000000000001"; --4
                         o_address <= current_address;
                         state <= WAIT_CLOCK_CICLE;
                    
@@ -103,22 +98,17 @@ begin
                         o_address <= current_address;
                         state <= WAIT_CLOCK_CICLE;
                    
-                      elsif(conv_integer(current_address)>= N_RIGHE * N_COLONNE +5 ) then 
+                      elsif(conv_integer(current_address)>= N_RIGHE * N_COLONNE +5 ) then --Se è stata letta tutta l'immagine
                         state <= CALC_AREA;
                       end if;
                       
-                when WAIT_CLOCK_CICLE=>
+                when WAIT_CLOCK_CICLE=> --Stato "vuoto" per attendere che la memoria invii i dati 
                      state <= LETTURA_BYTE;
              
-                when LETTURA_BYTE =>
-                    --report "stato: LETTURA_BYTE indirizzo:";
-                    --report integer'image(conv_integer(current_address));
-                    
+                when LETTURA_BYTE => --Lettura dei dati in arrivo dalla memoria. il controllo dell'indirizzo currente permette di stabilire quale variabile inizializzare
                     --LETTURA N_COLONNE
                      if( conv_integer(current_address)= 2) then      
                         N_COLONNE := conv_integer(i_data);
-                        --report "numero colonne: ";
-                        --report integer'image(N_COLONNE);
                         o_en <= '0';
                         if(N_COLONNE = 0) then
                             state <= CALC_AREA;
@@ -130,8 +120,6 @@ begin
                      --LETTURA N_RIGHE
                      if( conv_integer(current_address) = 3) then
                         N_RIGHE := conv_integer(i_data);
-                        --report "numero righe: ";
-                        --report integer'image(N_RIGHE);
                         o_en <= '0';
                         if(N_RIGHE = 0) then
                             state <= CALC_AREA;
@@ -143,8 +131,6 @@ begin
                      --LETTURA SOGLIA
                      if( conv_integer(current_address) = 4) then                     
                         SOGLIA := conv_integer(i_data);
-                        --report "numero soglia: ";
-                        --report integer'image(SOGLIA);
                         o_en <= '0';
                         if(SOGLIA = 0) then
                             state <= CALC_AREA;
@@ -156,76 +142,56 @@ begin
                      --LETTURA PIXEL
                      if( conv_integer(current_address)> 4) then                     
                         pixel_corrente := conv_integer(i_data);
-                        --report "valore pixel: ";
-                        --report integer'image(pixel_corrente);
                         o_en <= '0';
                         state <= CALC_COLONNA;
                      end if;
                      
-                when CALC_COLONNA =>
+                when CALC_COLONNA => --Sotto-stato che tiene traccia della colonna corrente. Aggiunto nel design finale per diminuire le operazioni eseguite da CHECK_PIXEL
                     curr_colonna:=curr_colonna + 1;
                     if(curr_colonna > (N_COLONNE - 1)) then
                         curr_colonna:=0;
                     end if;
-                
-                    --  report "stato: CHECK_PIXEL ";
-                    --curr_colonna:=0;
-                    --curr_colonna:=(conv_integer(current_address)-5) mod N_COLONNE;
-                    
-                    --report "n colonna: ";
-                    --report integer'image(curr_colonna);
                     state <= CALC_RIGA;
                 
-                when CALC_RIGA =>
+                when CALC_RIGA =>  --Sotto-stato che tiene traccia della riga corrente. Aggiunto nel design finale per diminuire le operazioni eseguite da CHECK_PIXEL
                     if(curr_colonna=0) then
                          curr_riga:=curr_riga+1;
                     end if;
-                    --curr_riga:=(conv_integer(current_address)- 5 -curr_colonna)/N_COLONNE;
-                    --report "n riga: ";
-                    --report integer'image(curr_riga);
                     state <= CHECK_PIXEL;
                     
-                when CHECK_PIXEL =>
-                    --report "stato: CHECK_PIXEL ";
+                when CHECK_PIXEL => --Controlla se il pixel acquisito fa parte dell'immagine
                     if (pixel_corrente >= SOGLIA) then
-                        state <= CHECK_X_MIN;
+                        state <= CHECK_X_MIN; --Se è nell'immagine si cercano in sequenza i valori x_min, x_max, y_min, y_max, ovvero le righe e colonne più esterne che appartengono all'immagine
                     else 
                         state <= AUMENTA_INDIRIZZO;   
                     end if;
-                    
+
+                --Serie di Sotto-stati cercano in sequenza i valori x_min, x_max, y_min, y_max. Aggiunti nel design finale per diminuire le operazioni eseguite da CHECK_PIXEL
                 when CHECK_X_MIN =>  
                     if (curr_riga < x_min) then
                         x_min:=curr_riga;
-                        --report "valore x_min: ";
-                        --report integer'image(x_min);
                     end if;
                     state <= CHECK_X_MAX;
 
                 when CHECK_X_MAX =>  
                     if (curr_riga > x_max) then
                         x_max:=curr_riga;
-                        --report "valore x_max: ";
-                        --report integer'image(x_max);
                     end if;
                     state <= CHECK_Y_MIN;
 
                 when CHECK_Y_MIN =>  
                     if (curr_colonna < y_min) then
                         y_min:=curr_colonna;
-                        --report "valore y_min: ";
-                        --report integer'image(y_min);
                     end if;
                     state <= CHECK_Y_MAX;         
                 
                 when CHECK_Y_MAX =>  
                      if (curr_colonna > y_max) then
                         y_max:=curr_colonna;
-                        --report "valore y_max: ";
-                        --report integer'image(y_max);
                      end if;
                     state <= AUMENTA_INDIRIZZO;               
                                     
-                when CALC_AREA =>
+                when CALC_AREA => --Decide come verrà calcolata l'area
                     report "stato: CALC_AREA ";
                     --Calcolo Area
                     if(SOGLIA = 0) then
@@ -238,48 +204,44 @@ begin
                     else
                         state <= CALC_ALTEZZA;
                     end if;
---                    report "valore area: ";
---                    report integer'image(conv_integer(area));
                 
-                when CALC_ALTEZZA =>
+                when CALC_ALTEZZA => --Calcola l'altezza a partire dai valori di riga di x_max e x_min - Sotto-stato aggiunti nel design finale per diminuire le operazioni svolte da CALC_AREA
                     altezza:=x_max-x_min+1;
                     state <= CALC_LUNGHEZZA;
                 
-                when CALC_LUNGHEZZA =>
+                when CALC_LUNGHEZZA => --Calcola la lunghezza a partire dai valori di riga di y_max e y_min - Sotto-stato aggiunti nel design finale per diminuire le operazioni svolte da CALC_AREA
                      lunghezza:=y_max-y_min+1;
                      state <= MOLT_ALTEZZA_LUNGHEZZA;
                 
-                when MOLT_ALTEZZA_LUNGHEZZA =>
+                when MOLT_ALTEZZA_LUNGHEZZA => --Calcolo finale dell'area - Operazione svolta in uno stato separato poichè particolarmente gravosa
                     area_int:= altezza*lunghezza;
                     state <= CONV_AREA_TO_BIT;
                 
-                when CONV_AREA_TO_BIT =>
+                when CONV_AREA_TO_BIT => --Sotto-stato che converte l'area (intero) in std_logic_vector
                     area:=std_logic_vector(to_unsigned(area_int, 16));
                     state <= MSB_WRITE;
                     
-                when MSB_WRITE =>
+                when MSB_WRITE => --Scrittura in memoria del byte più siginificativo
                     o_en <= '1';
                     o_we <= '1';
-                    
-                    --Scrittura Byte più significativo
                     o_address<="0000000000000001"; 
                     o_data <= area (15 downto 8);
                     state <= LSB_WRITE;
                     
-               when LSB_WRITE =>
-                    --Scrittura Byte meno significativo
+               when LSB_WRITE => --Scrittura in memoria del byte meno siginificativo
                     o_address <= "0000000000000000"; 
                     o_data <= area (7 downto 0);
                     state <= DONE_HIGH;
+               
+               --Stati che alzano il segnale di fine per un ciclo di clock
+               when DONE_HIGH =>
+                    o_en <= '0';
+                    o_done <= '1';
+                    state <= DONE_LOW;
                     
-                when DONE_HIGH =>
-                     o_en <= '0';
-                     o_done <= '1';
-                     state <= DONE_LOW;
-                    
-                when DONE_LOW =>
-                    --report "stato: DONE ";
-                    o_done <= '0';
+               when DONE_LOW =>
+                   o_done <= '0';
+                   state <= START_WAIT; --Riporta il componente allo stato iniziale pronto per eseguire un'altra operazione
                     
            end case;
         end if;
